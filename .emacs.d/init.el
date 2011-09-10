86568e1811ca2023b8b5a5beb0e71e0036c339c9
Sopin Dmitry <sopindm@gmail.com>
2011-09-10 07:06:31 +0400

    Настройки SBCL и Awesome.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,7 @@
           '(lambda ()
             (c-set-style "stroustrup")
 	    (setq c-basic-offset 2)))
+
 (setq inferior-lisp-program "/usr/bin/clisp")
 (setq slime-net-coding-system 'utf-8-unix)
 (slime-setup '(slime-scratch slime-fancy slime-asdf slime-references)) 
@@ -9,3 +10,10 @@
 				   (sbcl ("sbcl"))
 				   (clozurecl ("ccl"))
 				   (ecl ("ecl") :coding-system iso-8859-1-unix)))
+
+(defun refresh-file ()
+    (interactive)
+      (revert-buffer t t t)
+        )
+
+(global-set-key [f5] 'refresh-file)
